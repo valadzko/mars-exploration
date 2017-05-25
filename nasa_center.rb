cd13be4f642400c95a2eeeb0fe5b93b0e3c491e5
valadzko <pavel.valadzko@gmail.com>
2017-05-25 08:49:54 +0300

    Add simple input for one rover

--- nasa_center.rb
+++ nasa_center.rb
@@ -3,17 +3,24 @@ require "./rover.rb"
 
 class NasaCenter
 
-  def self.explore_mars!
-    # todo
+  def self.input_data
+    puts "Please input upper-right coordinates of the plateau, divided by space (default:5 5)"
+    @plateau_x, @plateau_y = gets.chomp.split().map { |e| e.to_i  }
+    puts "Please enter rover landing coordinates and direction code('N','E','S','W')"
+    @rover_x, @rover_y, @rover_direction_code = gets.chomp.split()
+    puts "Please enter rover route"
+    @rover_route = gets.chomp
+  end
 
-    # initialize Plato, Rovers
-    # deploy rovers on the Plato
-    # sequentially explore the plato with routes for rovers
-    @plateau = Plateau.new(5,5)
-    @rover = Rover.new(1,3,'N')
+  def self.explore_mars!
+    input_data
+
+    @plateau = Plateau.new(@plateau_x, @plateau_y)
+    @rover = Rover.new(@rover_x.to_i, @rover_y.to_i, @rover_direction_code)
 
     @rover.deploy(@plateau)
-    @rover.explore!("LMLMLMLMM")
-    true
+    @rover.explore!(@rover_route)
   end
 end
+
+NasaCenter.explore_mars!
